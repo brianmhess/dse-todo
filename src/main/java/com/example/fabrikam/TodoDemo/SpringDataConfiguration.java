3d7c9bd0df825410847365fa2202e34ebd17556b
brianmhess <brianmhess@gmail.com>
2018-11-13 22:35:20 +0000

    it's alive

--- src/main/java/com/example/fabrikam/TodoDemo/SpringDataConfiguration.java
+++ src/main/java/com/example/fabrikam/TodoDemo/SpringDataConfiguration.java
@@ -8,6 +8,7 @@ import org.springframework.data.cassandra.config.AbstractCassandraConfiguration;
 import org.springframework.data.cassandra.config.SchemaAction;
 import org.springframework.data.cassandra.repository.config.EnableCassandraRepositories;
 
+/*
 @Configuration
 @EnableCassandraRepositories
 public class SpringDataConfiguration extends AbstractCassandraConfiguration {
@@ -30,34 +31,46 @@ public class SpringDataConfiguration extends AbstractCassandraConfiguration {
     @Value("${dse.password}")
     public Optional < String > dsePassword;
   
-    /** {@inheritDoc} */
-  @Override
-  protected String getKeyspaceName() {
-      return keySpace;
-  }
+    // {@inheritDoc} 
+    @Override
+    protected String getKeyspaceName() {
+	return keySpace;
+    }
 
-    /** {@inheritDoc} */
-  @Override
-  protected String getContactPoints() {
-      return contactPoints;
-  }
+    // {@inheritDoc} 
+    @Override
+    protected String getContactPoints() {
+	return contactPoints;
+    }
 
-    /** {@inheritDoc} */
-  @Override
-  protected int getPort() {
-      return port;
-  }
+    // {@inheritDoc} 
+    @Override
+    protected int getPort() {
+	return port;
+    }
 
-    /** {@inheritDoc} */
-  @Override
-  public SchemaAction getSchemaAction() {
-      return SchemaAction.CREATE_IF_NOT_EXISTS;
-  }
+    // {@inheritDoc} 
+    @Override
+    public SchemaAction getSchemaAction() {
+	return SchemaAction.CREATE_IF_NOT_EXISTS;
+    }
 
-    /** {@inheritDoc} */
-  @Override
-  public String[] getEntityBasePackages() {
-      return new String[] {basePackages};
-  }
+    // {@inheritDoc} 
+    @Override
+    public String[] getEntityBasePackages() {
+	return new String[] {basePackages};
+    }
   
+}
+*/
+
+@Configuration
+public class SpringDataConfiguration extends AbstractCassandraConfiguration {
+    public String getContactPoints() {
+	return "node0";
+    }
+
+    public String getKeyspaceName() {
+	return "demo_sdc";
+    }
 }
